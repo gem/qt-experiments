2816c00d461651e06e6170a44394724edae8b5e5
Marco Bernasocchi <marco@opengis.ch>
2014-12-04 18:43:15 +0100

    better debug possibility

--- svir/import_sv_data.py
+++ svir/import_sv_data.py
@@ -221,8 +221,8 @@ class SvDownloaderWorker(QObject):
     finished = pyqtSignal(bool)
 
     def fake_run(self):
-        total = 2
-        for i in range(total):
+        total = 5
+        for i in range(1, total+1):
             if self.is_aborted:
                 raise SvDownloadAborted
             sleep(1)
